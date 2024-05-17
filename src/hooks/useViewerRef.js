7a59dae99484ba7b9382a2c19eaa68a6a8a233d7
eunyeongBae <adcadcad@naver.com>
2024-05-17 14:10:09 +0900

    useViewerRef hook 함수 내 viewer instance 생성하는 로직 추가

--- src/hooks/useViewerRef.js
+++ src/hooks/useViewerRef.js
@@ -1,9 +1,20 @@
 import { useEffect, useRef } from "react";
-import TilesetViewer from "../componenets/TilesetViewer";
+import { CesiumTerrainProvider, Viewer } from "cesium";
 
 //viewer 하나만 띄우고 유지하기 위해서
 const useViewerRef = () => {
     const viewerRef = useRef(null);
+
+    const TilesetViewer = async() => {
+        const viewer = new Viewer ("cesiumContainer", {
+                terrainProvider: await CesiumTerrainProvider.fromIonAssetId(
+                1,
+            )
+        });
+        viewer.scene.globe.depthTestAgainstTerrain = true;
+      
+        return viewer;
+    };
 
     useEffect(() => {
         const initializeViewer = async() => {
