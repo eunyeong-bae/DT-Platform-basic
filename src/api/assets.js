065a0a56813ac1d2f15f9d4276f1098f3dc1fcfc
eunyeongBae <adcadcad@naver.com>
2024-05-24 13:48:24 +0900

    self-hosted api 호출 로직 추가

--- src/api/assets.js
+++ src/api/assets.js
@@ -1,18 +1,33 @@
 import axios from "axios";
 
-const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxMGFlMjAxNi1iMWJhLTRkN2MtOTYzYy1iMGY2YTc5Yzg1YTkiLCJpZCI6MjEyNjkyLCJpYXQiOjE3MTUzMDI0MDJ9.s678GHASYCJ8H8fyyTb79jsnFaDrWh-o7Xe8ig0XDqs';
+// const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxMGFlMjAxNi1iMWJhLTRkN2MtOTYzYy1iMGY2YTc5Yzg1YTkiLCJpZCI6MjEyNjkyLCJpYXQiOjE3MTUzMDI0MDJ9.s678GHASYCJ8H8fyyTb79jsnFaDrWh-o7Xe8ig0XDqs'
 
 export const getAssets = async() => {
     try{
         // const accessToken = process.env.REACT_APP_ACCESS_TOKEN;
-        const response = await axios.get('https://api.cesium.com/v1/assets', {
+        const response = await axios.get('http://172.18.247.14:31587/v1/assets',{
             baseURL: 'https://api.cesium.com/v1/assets',
-            headers: { Authorization: `Bearer ${accessToken}` },
-        })
-        // console.log("dd", response.data)
+            // headers: { Authorization: `Bearer ${accessToken}` },
+        });
+        // console.log("success self hosted data : 121212", response.data)
         return response.data;
         
     }catch(error) {
         console.log("ddd",error)
     }
 }
+
+// export const getAssets = async() => {
+//     try{
+//         // const accessToken = process.env.REACT_APP_ACCESS_TOKEN;
+//         const response = await axios.get('https://api.cesium.com/v1/assets', {
+//             baseURL: 'https://api.cesium.com/v1/assets',
+//             headers: { Authorization: `Bearer ${accessToken}` },
+//         })
+//         // console.log("dd", response.data)
+//         return response.data;
+        
+//     }catch(error) {
+//         console.log("ddd",error)
+//     }
+// }
